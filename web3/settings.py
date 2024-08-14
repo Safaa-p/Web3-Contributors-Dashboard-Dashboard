341caaa90cbd24a2868d723967d13ffc289dc606
Safaa-p <111049448+Safaa-p@users.noreply.github.com>
2024-08-14 14:15:54 +0100

    Update settings.py

--- web3/settings.py
+++ web3/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': '',
-        'PASSWORD': ',
+         'USER':config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'aws-0-eu-central-1.pooler.supabase.com',  
         'PORT': '6543',          
     }
